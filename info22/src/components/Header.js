9b0285f9e08dc395e5ecafef0e43ea424b81f29e
louisrubin <miguel10.rbn@gmail.com>
2022-06-01 10:43:52 -0300

    finish Header component

--- info22/src/components/Header.js
+++ info22/src/components/Header.js
@@ -4,9 +4,16 @@ import "./Estilos.css";
 class Header extends Component {
     render(){
         return (
-            <div className='header'>
-                <h1>React & Material-UI Sample Application</h1>
-            </div>
+            <>
+                <div className='header'>
+                    <h1>React & Material-UI Sample Application</h1>
+                    
+                </div>
+                <div>                        
+                    <input className='search' placeholder='Search for Courses'/>
+                </div>
+
+            </>
         )
     }
 }
