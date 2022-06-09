2781ffae7389d93be6c1201325e9120e870b7a91
louisrubin <miguel10.rbn@gmail.com>
2022-06-09 18:54:17 -0300

    images to Cards. font-sizes changed

--- info22/src/components/Card.js
+++ info22/src/components/Card.js
@@ -1,9 +1,10 @@
 import { Component } from "react";
 import "./Estilos.css";
-import cuarteto from "../assets/el-cuarteto-de-god.jpg";
+import react_redux from "../assets/ignore/React-and-Redux.jpg";
+import react_cool from "../assets/ignore/react-cool.jpeg";
+import react_violet from "../assets/ignore/react-violet.jpg";
+import enzo from "../assets/ignore/enzo-te-elimino.PNG";
 
-
-const enzo = require("../assets/get-back-beatles.jpg").default;
     
 class Card extends Component {
 
@@ -14,29 +15,29 @@ class Card extends Component {
     render(){
         return (
             <>
-            <div className="Card-content">
-                <div className="card">
-                    <img src={cuarteto} />
-                    <div className="content">
+            <div className="whole-Cards">
+                <div className="Card">
+                    <img src={react_redux} />
+                    <div className="Card-content">
                         <h4>Server Side Rendering with React and Redux</h4>
                         <p>Build React, Redux, and React apps using Server Side Rendering (SSR), Isomorphic, and Universal JS techniques</p>
                         <a href="#">Go to course</a>
                     </div>
                     
                 </div>
-                <div className="card">
-                    <img src={cuarteto} />
-                    <div className="content">
-                        <h4>Server Side Rendering with React and Redux</h4>
-                        <p>Build React, Redux, and React apps using Server Side Rendering (SSR), Isomorphic, and Universal JS techniques</p>
+                <div className="Card">
+                    <img src={react_cool} />
+                    <div className="Card-content">
+                        <h4>React - The Complete Guide</h4>
+                        <p>Dive in and learn React from scratch! Learn Reactjs, Redux, React Routing, Animations, Next.js basics and way more!</p>
                         <a href="#">Go to course</a>
                     </div>
                 </div>
-                <div className="card">
-                    <img src={cuarteto} />
-                    <div className="content">
-                        <h4>Server Side Rendering with React and Redux</h4>
-                        <p>Build React, Redux, and React apps using Server Side Rendering (SSR), Isomorphic, and Universal JS techniques</p>
+                <div className="Card">
+                    <img src={react_violet} />
+                    <div className="Card-content">
+                        <h4>The complete React Web Developer Course (with Redux)</h4>
+                        <p>Learn how to build and launch React web applications using React v16, Redux, Webpack, React-Router v4, and more!</p>
                         <a href="#">Go to course</a>
                     </div>
                 </div>
