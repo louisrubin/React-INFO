fe421f242ed2605dd263c5632308529456ac97c7
louisrubin <miguel10.rbn@gmail.com>
2022-06-28 11:31:49 -0300

    'user.controller' custom module

--- exercises React/api/api.js
+++ exercises React/api/api.js
@@ -1,30 +1,16 @@
 const express = require('express')  // require importa dependencias. 'express' nombre de la librería
+const user = require('./user.controller')
 const app = express()   // -app- se crea partir de la ejecución de la función 'express'
 const port = 3000
 
 
 //              request, response
-app.get('/',    (req,     res) => {
-    res.status(200).send('Chanchito feliz')
-})
-
-app.post('/', (req, res) => {
-    res.status(201).send('creando chanchito')
-})
-
-app.put('/:id', (req, res) => {
-    res.sendStatus(204)
-})
-
-app.patch('/:id', (req, res) => {
-    res.sendStatus(204)
-})
-
-app.delete('/:id', (req, res) => {
-    res.sendStatus(204)
-})
-
-
+app.get('/', user.list)
+app.post('/', user.create)
+app.get('/:id', user.get)
+app.put('/:id', user.update)
+app.patch('/:id', user.update)
+app.delete('/:id', user.destroy)
 
 app.listen(port, () => {
     console.log('arrancando la app.')
