7aa2ed1bff632f5e3f20a6d6ec6b9e03d86b7f9e
louisrubin <miguel10.rbn@gmail.com>
2022-06-28 13:37:25 -0300

    middleware JSON. body request

--- exercises React/api/api.js
+++ exercises React/api/api.js
@@ -1,7 +1,15 @@
 const express = require('express')  // require importa dependencias. 'express' nombre de la librería
+const mongoose = require('mongoose')    // mongoose
 const user = require('./user.controller')
 const app = express()   // -app- se crea partir de la ejecución de la función 'express'
 const port = 3000
+
+app.use(express.json())     /* Esto toma todas las peticiones que vengan en un formato JSON, los transforma en un objeto JavaScript y los asigna a la propiedad body     
+                             Esto es un MIDDLEWARE: se ejecuta cuando realizamos cualquier tipo de peticion en la app, o sea siempre
+                                    se utilizan generalmente para realizar Validaciones, como en este caso; sacar los datos que vienen
+                                    a través de una peticion POST e inyectarlos en la propiedad de 'body' de nuestro objeto 'request'
+                           */
+mongoose.connect('mongodb+srv://louis-rbn:admin123@cluster0.oyqdl0d.mongodb.net/miapp?retryWrites=true&w=majority')
 
 
 //              request, response
--- exercises React/api/user.controller.js
+++ exercises React/api/user.controller.js
@@ -1,17 +1,21 @@
+const Users = require('./User')
+
 const User = {
-    get: (req, res) => {
+    get: async (req, res) => {
         res.status(200).send('Este es un chanchito')
     },    
-    list: (req, res) => {
-        res.status(200).send('Hola Chanchito!')
+    list: async (req, res) => {
+        const users = await Users.find()
+        res.status(200).send(users)
     },
-    create: (req, res) => {
+    create: async (req, res) => {
+        console.log(req.body)   // los datos de las peticiones POST vienen en 'body'
         res.status(201).send('Creando un Chanchito')
     },
-    update: (req, res) => {
+    update: async (req, res) => {
         res.status(204).send('Actualizando Chanchito')
     },
-    destroy: (req, res) => {
+    destroy: async (req, res) => {
         res.status(204).send('Eliminando un chanchito :(')
     }
 }
