77d28b5857063095a72aa03166dcc61c7d3a452d
louisrubin <miguel10.rbn@gmail.com>
2022-07-01 12:23:40 -0300

    listing users => main.js

--- exercises React/api/app/main.js
+++ exercises React/api/app/main.js
@@ -23,6 +23,17 @@ const loadInitialTemplate = () => {
 }
 
 const getUsers = async () => {
+    const response = await fetch('/users')
+    const users = await response.json()     // la respuesta que nos devuelve -fetch- dentro de const 'response' cuenta con un metodo json() el cual nos permite transformar esas respuestas en un -objeto- javascript
+    const template = user => `
+        <li>
+            ${user.name} ${user.lastname} <button data-id="${user._id}">Eliminar</button>
+        </li>
+    `
+    const userList = document.getElementById('user-list')
+    //              con map() iteramos, recibe un parámetro 'user' el cual ejecuta la funcion 'template()' el cual espera por parámetro un usuario, asi que le pasamos el mismo usuario
+    userList.innerHTML = users.map(user => template(user)).join('')
+    // por cada 'user' devuelto por el servidor, ejecuta la funcion 'template()'. Esto devuelve un arreglo [ ] donde se contiene la plantilla de ese user, por eso llamamos al método join() para convertir todo eso en un 'string' separados por ningún caracter: .join('')
 
 }
 
@@ -53,6 +64,7 @@ window.onload = () => {
     // window es un objeto global que hace referencia a nuestra ventana del explorador
     // onload() es la función que se va a ejecutar una vez que se cargue TODO el contenido en la ventana
     loadInitialTemplate()
-    addFormListener()
+    addFormListener()   // ejecuta el formulario
+    getUsers()      // una vez cargado la página ejecuta getUsers() el cual imprime todos los usuarios existentes
 
 }
